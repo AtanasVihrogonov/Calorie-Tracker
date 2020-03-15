f37883dc5f87103de3b0b0014596e53b3db695d3
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-15 19:03:13 +0000

    Data structure

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // Storage Controler
 
 // Item Controller
-const ItemControler = (function() {
+const ItemController = (function() {
   // Item Constructor
   const Item = function(id, name, calories) {
-    
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Avocado', calories: 400 },
+      { id: 0, name: 'Eggs', calories: 300 },
+      { id: 0, name: 'Lettuce', calories: 200 }
+    ],
+    currentItem: null,
+    totalCalories: 0
+
   }
 })();
 
