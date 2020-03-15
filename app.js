e6ab3408be9f20e4d165e3fe673f6d9dda761432
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-15 18:05:07 +0000

    Basic markup

---
+++ app.js
@@ -0,0 +1,19 @@
+// Storage Controler
+
+// Item Controller
+const ItemControler = (function() {
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    
+  }
+})();
+
+// UI Controler
+const UIController = (function() {
+
+})();
+
+// App Controler
+const AppController = (function(ItemController, UIController) {
+
+})(ItemController, UIController);
